52288e226e2d30aa7754a417e877dee9ce4bd5dc
renandantasp <renandantasp@gmail.com>
2023-07-16 23:11:40 -0300

    setup colors, adjust media queries on content sections

--- astro/src/env.d.ts
+++ astro/src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+type Palette = {
+  main_color : string
+  secondary_color : string
+  accent_color : string
+  gradient_from : string
+  gradient_to : string
+
+}
